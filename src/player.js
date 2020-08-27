f81a259e6d97792d5d9e32c33d854289ce8d2aef
arrow2nd <arrowsub57@gmail.com>
2020-08-27 12:18:51 +0900

    fix: 画像がドラッグできるバグを修正

--- src/player.js
+++ src/player.js
@@ -2,83 +2,80 @@
 const app = window.app;
 const dialog = window.dialog;
 const shell = window.shell;
-
 const audioElem = new Audio();
-const inputArea = $('#inputArea');
-const playButton = $('#playButton');
-const prevButton = $('#prevButton');
-const nextButton = $('#nextButton');
-const searchButton = $('#searchButton');
-const shuffleButton = $('#shuffleButton');
-const exitButton = $('#exitButton');
+const form = $('#inputArea');
+const playBtn = $('#playButton');
+const prevBtn = $('#prevButton');
+const nextBtn = $('#nextButton');
+const searchBtn = $('#searchButton');
+const shuffleBtn = $('#shuffleButton');
+const exitBtn = $('#exitButton');
 const seekbar = $('#seekbar');
 let seek;
 
-// シークバーの位置を更新
-const seekbarUpdate = () => {
-    const now = Math.round(audioElem.currentTime);
-    seekbar.val(now);
-};
+
+// 画像のドラッグを禁止
+$(function () {
+    $('.copyProtection').attr('onmousedown', 'return false');
+    $('.copyProtection').attr('onselectstart', 'return false');
+});
 
 // 最新回を取得
-let latestRadioNumber = 200; // 取得に失敗した時に使用
+let latestRadioNum = 200;
 $.ajax({
     type: 'GET',
     url: 'https://omocoro.jp/tag/%E5%8C%BF%E5%90%8D%E3%83%A9%E3%82%B8%E3%82%AA/'
 }).done((data) => {
     const title = $(data).find('.title')[0];
-    const text =$(title).text();
+    const text = $(title).text();
     const indexOfFirst = text.indexOf('【');
-    if (indexOfFirst === -1) {
+    if (indexOfFirst == -1) {
         dialog.showErrorBox('エラー', 'データの解析に失敗しました');
     } else {
-        latestRadioNumber = Number(text.substr(indexOfFirst + 1, 3));
+        latestRadioNum = Number(text.substr(indexOfFirst + 1, 3));
     };
-    inputArea.val(latestRadioNumber);
-}).fail((error) => {
+    form.val(latestRadioNum);
+}).fail(() => {
     dialog.showErrorBox('エラー', '最新回の取得に失敗しました');
 });
 
+
 // 再生停止ボタン
-playButton.on('click', () => playControl());
+playBtn.on('click', () => control());
 
 // 入力エリアでEnter
-inputArea.on('keypress', (e) => {
-    if (e.which === 13) {
-        playControl();
+form.on('keypress', (e) => {
+    if (e.which == 13) {
+        control();
     };
 });
 
 // 前へボタン
-prevButton.on('click', () => prevTrack());
+prevBtn.on('click', () => prevTrack());
 
 // 次へボタン
-nextButton.on('click', () => nextTlack());
+nextBtn.on('click', () => nextTlack());
 
 // 検索ボタン
-searchButton.on('click', () => {
-    const nowPlaying = inputArea.val() == 0 ? '001' : ('000' + inputArea.val()).slice(-3);
+searchBtn.on('click', () => {
+    const nowPlaying = (form.val() == 0) ? '001' : ('000' + form.val()).slice(-3);
     shell.openExternal(`https://omocoro.jp/?s=${decodeURIComponent(`【${nowPlaying}】ARuFa・恐山の匿名ラジオ`)}`);
 });
 
-// シャッフルボタン 
-shuffleButton.on('click', () => {
-    if (shuffleButton.css('filter') === 'brightness(0.5)') {
-        shuffleButton.css({'filter': 'brightness(1)'});
+// シャッフルボタン
+shuffleBtn.on('click', () => {
+    if (shuffleBtn.css('filter') == 'brightness(0.5)') {
+        shuffleBtn.css({'filter': 'brightness(1)'});
     } else {
-        shuffleButton.css({'filter': 'brightness(0.5)'});
+        shuffleBtn.css({'filter': 'brightness(0.5)'});
     };
 });
 
 // 終了ボタン
-exitButton.on('click', () => {
-    app.quit(); 
-});
+exitBtn.on('click', () => app.quit());
 
 // シーク中
-seekbar.on('mousedown', () => {
-    clearInterval(seek);
-});
+seekbar.on('mousedown', () => clearInterval(seek));
 
 // シーク反映
 seekbar.on('change', () => {
@@ -86,72 +83,80 @@ seekbar.on('change', () => {
     audioElem.currentTime = seekOfs;
 });
 
-
 // 再生可能（開始した）
 audioElem.oncanplaythrough = () => {
-    if (playButton.attr('src') === '../img/pause.png') {
+    if (playBtn.attr('src') == '../img/pause.png') {
         const seekbarMax = Math.round(audioElem.duration);
         seekbar.attr('max', seekbarMax);
         clearInterval(seek);
-        seek = setInterval(seekbarUpdate, 1000);
+        seek = setInterval(updateSeekbar, 1000);
     };
 };
 
 // 再生終了
 audioElem.onended = () => {
     // 次の回を指定
-    if (shuffleButton.css('filter') === 'brightness(0.5)') {
+    if (shuffleBtn.css('filter') == 'brightness(0.5)') {
         nextTlack();
     } else {
-        const randomNo = Math.ceil(Math.random() * latestRadioNumber);
-        inputArea.val(randomNo);
+        const randomNo = Math.ceil(Math.random() * latestRadioNum);
+        form.val(randomNo);
     };
     stopRadio();
-    const nowRadioNumber = Number(inputArea.val());
+    const nowRadioNumber = Number(form.val());
     playRadio(nowRadioNumber);
 };
 
-// ファイルが存在しない
+// 存在しない
 audioElem.onerror = () => {
-    const nowRadioNumber = Number(inputArea.val());
+    const nowRadioNumber = Number(form.val());
     dialog.showErrorBox('再生できません', `匿名ラジオ 第${nowRadioNumber}回は見つかりませんでした`);        
-    playButton.attr('src', '../img/play.png');
+    playBtn.attr('src', '../img/play.png');
     stopRadio();
 };
 
 
 /**
+ * シークバーの位置を更新
+ */
+const updateSeekbar = () => {
+    const now = Math.round(audioElem.currentTime);
+    seekbar.val(now);
+};
+
+/**
  * 数字入力フォームの値を制限
- * @param {*} element 要素
+ * @param {Object} element 要素
  */
 function limitValue(element) {  
     const value = Number(element.value.slice(0, 3));
-    if (element.value === 0 ) {
-        element.value = 1;
-    };
-    element.value = value>latestRadioNumber ? latestRadioNumber : value;
+    element.value = (latestRadioNum < value) ? latestRadioNum : value;
 };
 
 /**
  * 再生・停止振り分け
  */
-function playControl() {
-    if (playButton.attr('src') === '../img/pause.png') {
-        playButton.attr('src', '../img/play.png');
+function control() {
+    if (playBtn.attr('src') == '../img/pause.png') {
+        playBtn.attr('src', '../img/play.png');
         stopRadio();
         return;
     };
-    playButton.attr('src', '../img/pause.png');
-    const nowRadioNumber = Number(inputArea.val());
-    playRadio(nowRadioNumber);
+    playBtn.attr('src', '../img/pause.png');
+    let radioNum = Number(form.val());
+    if (radioNum == 0) {
+        radioNum = 1;
+        form.val(1);
+    };
+    playRadio(radioNum);
 };
 
 /**
  * 再生開始
- * @param {*} number 再生する回
+ * @param {Number} number 再生する回
  */
 function playRadio(number) {
-    const radioNo = number == 0 ? '001' : ('000' + number).slice(-3);
+    const radioNo = (number == 0) ? '001' : ('000' + number).slice(-3);
     const url = `https://omocoro.heteml.net/radio/tokumei/${radioNo}.mp3`;
     if (audioElem.src !== url) {
         audioElem.src = url;
@@ -160,7 +165,7 @@ function playRadio(number) {
         const nowSeekOfs = Math.round(audioElem.currentTime);
         seekbar.val(nowSeekOfs);
         clearInterval(seek);
-        seek = setInterval(seekbarUpdate, 1000);
+        seek = setInterval(updateSeekbar, 1000);
     };
     audioElem.play();
 };
@@ -177,17 +182,18 @@ function stopRadio() {
  * 次のトラックを指定
  */
 function nextTlack() {
-    let nowRadioNumber = Number(inputArea.val());
-    nowRadioNumber = nowRadioNumber === 0 ? 1 : nowRadioNumber;
-    const radioNumber = nowRadioNumber + 1 > latestRadioNumber ? 1 : nowRadioNumber + 1;
-    inputArea.val(radioNumber);
+    let radioNumber = Number(form.val());
+    radioNumber++;
+    radioNumber = (latestRadioNum < radioNumber) ? 1 : radioNumber;
+    form.val(radioNumber);
 };
 
 /**
  * 前のトラックを指定
  */
 function prevTrack() {
-    const nowRadioNumber = Number(inputArea.val());
-    const radioNumber = nowRadioNumber - 1 < 1 ? latestRadioNumber : nowRadioNumber - 1;
-    inputArea.val(radioNumber);
+    let radioNumber = Number(form.val());
+    radioNumber--;
+    radioNumber = (radioNumber < 1) ? latestRadioNum : radioNumber;
+    form.val(radioNumber);
 };
